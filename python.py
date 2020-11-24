8b5430741f80a9eeb1eec753c623ded10ee85230
shuklaharsh911 <48179654+shuklaharsh911@users.noreply.github.com>
2020-11-25 01:50:55 +0530

    Update python.py

--- python.py
+++ python.py
@@ -4,98 +4,94 @@ import sys
 import asyncio
 import pandas as pd
 # import glob
-# import requests
+import requests
 import os
-RSI_COMP=[]
-Comp=[]
-forwardEps=[]
-trailEps=[]
-RS=[]
-
-# taking data price and time he/she wants to invest from the user.
-userInputPrice = sys.argv[1]
-userInputTime = sys.argv[2]
-
-async def run(stocksCodesList):
-    for stockCode in stocksCodesList:
-            #Get stock info
-            temp = yf.Ticker(stockCode)
-            data = temp.history(start="2020-01-01", end="2020-04-30")
-            y = temp.info
-            fEps = y["forwardEps"]
-            tEps = y["trailingEps"]
-            prices = []
-            c = 0
-            Hist_data = data
-
-            while c < len(Hist_data):
-                if Hist_data.iloc[c,4] > float(2.00):  # Check that the closing price for this day is greater than $2.00
-                    prices.append(Hist_data.iloc[c,4])
-                c += 1
-
-            i = 0
-            upPrices=[]
-            downPrices=[]
-             #  Loop to hold up and down price movements
-            while i < len(prices):
-                if i == 0:
-                     upPrices.append(0)
-                     downPrices.append(0)
-                else:
-                      if (prices[i]-prices[i-1])>0:
-                          upPrices.append(prices[i]-prices[i-1])
-                          downPrices.append(0)
-                      else:
-                          downPrices.append(prices[i]-prices[i-1])
-                          upPrices.append(0)
-                i += 1
-            x = 0
-            avg_gain = []
-            avg_loss = []
-   
-            while x < len(upPrices):
-                if x <15:
-                    avg_gain.append(0)
-                    avg_loss.append(0)
-                else:
-                    sumGain = 0
-                    sumLoss = 0
-                    y = x-14
-                    while y<=x:
-                        sumGain += upPrices[y]
-                        sumLoss += downPrices[y]
-                        y += 1
-                    avg_gain.append(sumGain/14)
-                    avg_loss.append(abs(sumLoss/14))
-                x += 1
-            p = 0
-            RSI = 0
-            p = len(prices) - 1
-            print(avg_gain[p])
-            RSvalue = (avg_gain[p]/avg_loss[p])
-            RSI = (100 - (100/(1+RSvalue)))
+import json
+f = open('data.json',)
+x = "http://localhost:8081/getCompanyData"
+data = json.load(f)
+cmp = []
+value = []
+df = {}
+for i in data['allCompanies']:
+    cal = [] 
+    url = x+i
+    response = requests.get(url)
+    d = response.text
+    parsed = json.loads(d)
+    ans = 0
+    cmpName = parsed["data"]["name"]
+    for l in parsed["data"]["data"]:
+        t = l["period"]
+        lvl = l["level"]
+        ind = l["indication"]
+        if(t == "MACD(12,26,9)"):
+            if(lvl > '0'):
+                cal.append("Bullish")
+            else:
+                cal.append("Bearish")
+                
+        if(t == "RSI(14)"):
+            if(lvl > '70'):
+                cal.append("Bearish")
+            elif(lvl < '30'):
+                cal.append("Bullish")
+            else:
+                cal.append("Neutral")
             
-            Comp.append(stockCode)
-            forwardEps.append(fEps)
-            trailEps.append(tEps)
-            RS.append(RSvalue)
-            RSI_COMP.append(RSI)
 
+        if(t == "Stochastic(20,3)"):
+            if(lvl > '80'):
+                cal.append("Bearish")
+            else:
+                cal.append("Bullish")
 
+        if(t == "ROC(20)"):
+             if(lvl > '0'):
+                cal.append("Bullish")
+             else:
+                cal.append("Bearish")
+            
 
+        if(t == "CCI(20)"):
+            if(lvl > '100'):
+                cal.append("Bearish")
+            elif(lvl > '100'):
+                cal.append("Bullish")
+            else:
+                cal.append("Bearish")
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run(["TTM"]))
-loop.close()
-df_dict = {
-    'Company' : Comp,
-    'forwardEps' : forwardEps,
-    'trailingEps' : trailEps,
-    'RS' : RS,
-    'RSI' : RSI_COMP
-}
-df = pd.DataFrame(df_dict, columns = ["Company", "forwardEps", "trailingEps", "RS", "RSI"])
-df.to_csv("file.csv", index = False)
+        
+        if(t == "RSC (6 months)"):
+            if(ind =="Outperformer"):
+                cal.append("Bullish")
+            else:
+                cal.append("Bearish")
 
-# sending the data to user through nodejs
-print("Tata Consultancy Services Ltd. : 95% \n Reliance Industries Ltd. : 91% \n HDFC Bank Ltd. : 70% \n ITC Ltd. : 59% \n")
+        
+        if(t == "ADX(14)"):
+            if(lvl > "25"):
+                cal.append("Bullish")
+            else:
+                cal.append("Bearish")
+        
+    for i in cal:
+        if(i == "Bullish"):
+            ans+=1
+    df[cmpName] = ans
+
+sort_orders = sorted(df.items(), key=lambda x: x[1], reverse=True)
+
+for i in sort_orders:
+	print(i[0], i[1])
+    
+    
+    
+
+        
+        
+      
+        
+    
+
+f.close()
